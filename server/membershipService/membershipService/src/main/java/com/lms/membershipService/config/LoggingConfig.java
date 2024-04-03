bdee4fb61f474111238fe779bc5b117de81e7f05
unknown <srivastavasakshii21@gmail.com>
2024-04-03 14:48:45 +0530

    members service done

---
+++ server/membershipService/membershipService/src/main/java/com/lms/membershipService/config/LoggingConfig.java
@@ -0,0 +1,11 @@
+package com.lms.membershipService.config;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class LoggingConfig {
+
+   
+}
