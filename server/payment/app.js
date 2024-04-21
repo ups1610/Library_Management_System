963c6535564dc69ba9d032b4cfbb8ed9a18f5abb
ups1610 <pratapsinghupendra96@gmail.com>
2024-04-21 10:53:22 +0530

    ADD: api to get payment gateway link

--- server/payment/app.js
+++ server/payment/app.js
@@ -1,31 +1,36 @@
 const express = require('express');
 const cors = require('cors');
+require('dotenv').config();
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
+
 const app = express();
-const stripe = require("stripe")("sk_test_51P7CTlSCKWbIqmQ9AoXp7JVPXHsd0MPuHd6i3D9f39OmHlaluV0HcwCt3HatzEUHVYvXNFmqyt6drrrxHAYTkwZO00OI6thl1H");
 
 app.use(express.json());
 app.use(cors());
 
+
+
 app.post("/api/create-checkout-session", async (req, res) => {
-    const { products } = req.body;
-
-    const lineItems = [{
-        price_data: {
-            currency: "inr",
-            product_data: {
-                name: products.member
-            },
-            unit_amount: products.amount * 100,
-        },
-        quantity: 1
-    }];
-
     try {
+        const product = req.body;
+
+        if (!product || !product.amount || !product.member) {
+            return res.status(400).json({ error: "Invalid request. Please provide product information." });
+        }
+
+        const price = await stripe.prices.create({
+            currency: 'inr',
+            unit_amount: product.amount * 100,
+            product_data: {
+                name: product.member
+            }
+        });
+
         const paymentLink = await stripe.paymentLinks.create({
-            line_items: lineItems,
-            mode: "payment",
-            success_url: "http://localhost:3000/dashboard/transactions",
-            cancel_url: "http://localhost:3000/dashboard/transactions",
+            line_items: [{
+                price: price.id,
+                quantity: 1,
+            }],
         });
 
         res.json({ url: paymentLink.url });
@@ -35,7 +40,7 @@ app.post("/api/create-checkout-session", async (req, res) => {
     }
 });
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 });
